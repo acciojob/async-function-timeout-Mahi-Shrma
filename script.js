335fbe94efc2fbbb1ceb94a6482a3820ffa6c6bc
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2024-10-31 03:33:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// const text= document.getElementById("text").value;
+// const num= document.getElementById("delay").value;
+const btn= document.getElementById("btn");
+const div= document.getElementById("output");
+
+btn.addEventListener('click', async()=>{
+	 const text = document.getElementById("text").value;
+    const num = parseInt(document.getElementById("delay").value);
+    await displayOutput(text,num);
+})
+ async function displayOutput(mes,delay){
+	 await new Promise(resolve=>setTimeout(resolve,delay));
+	 div.innerText=mes;
+ }
